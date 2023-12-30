3ab0f71f3ffd5257f17d90b94bae23f598147904
Jas0nt <taoeta@gmail.com>
2023-12-30 10:01:40 +0800

    console font and color

--- configuration.nix
+++ configuration.nix
@@ -37,6 +37,31 @@
   services.gvfs.enable = true; 
   services.udisks2.enable = true;
 
+  console = {
+    earlySetup = true;
+    packages = with pkgs; [ terminus_font ];
+    font = "${pkgs.terminus_font}/share/consolefonts/ter-114n.psf.gz";
+    keyMap = "us";
+    colors = [
+        "282a36"
+        "ff5555"
+        "50fa7b"
+        "f1fa8c"
+        "bd93f9"
+        "ff79c6"
+        "8be9fd"
+        "f8f8f2"
+        "6272a4"
+        "ff7777"
+        "70fa9b"
+        "ffb86c"
+        "cfa9ff"
+        "ff88e8"
+        "97e2ff"
+        "ffffff"
+    ];
+  };
+
   # Set your time zone.
   time.timeZone = "Asia/Shanghai";
 
