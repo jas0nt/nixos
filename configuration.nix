46561b023cce9a2a1a233d8d81621f04b5c44bb5
Jas0nT <taoeta@gmail.com>
2024-04-01 16:52:13 +0800

    a

--- configuration.nix
+++ configuration.nix
@@ -251,7 +251,7 @@
     gnumake libgccjit cmake
     openssl
     nmap
-    wl-clipboard
+    xclip wl-clipboard
 
     feh
     i3lock-fancy
@@ -312,7 +312,7 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
+  system.stateVersion = "23.11"; # Did you read the comment?
 
 
   # home-manager
